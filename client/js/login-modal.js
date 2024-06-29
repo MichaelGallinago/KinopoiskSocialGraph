5452cbdda0e00e72a1c443b8369289a75fc478c4
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2024-06-29 21:54:36 +0500

    Test authorization

--- client/js/login-modal.js
+++ client/js/login-modal.js
@@ -1,12 +1,13 @@
 function _createModal(options) {
     const modal = document.createElement('div')
+    modal.setAttribute('id', 'login-form')
     modal.classList.add('login-modal')
     modal.insertAdjacentHTML('afterbegin', `
         <div class="auth-overlay" data-close="true">
             <div class="auth-modal-window">
                 <img src="img/close-icon.png" alt="close-icon" class="close-icon" data-close="true">
                 <h2 class="login-title">Вход</h2>
-                <form action="" method="post" class="login-form auth-container">
+                <form id="login-form" class="login-form auth-container">
                     <div class="auth-field">
                         <img src="img/login-icon.png" alt="login-icon" class="login-icon auth-icon">
                         <input type="text" name="login" id="login-input" class="auth-input" required placeholder="Логин" />
--- client/js/main.js
+++ client/js/main.js
@@ -1,40 +1,74 @@
 const loginModal = $.modal()
-
 const openLoginWindow = document.getElementById('open-login-window')
 openLoginWindow.addEventListener('click', event => {
     loginModal.open()
 })
 
-// Регистрация
-const BASE_URL = "http://127.0.0.1:5000" // TODO: Установить URL
-const registerForm = document.getElementById('register-form')
+const BASE_URL = "http://127.0.0.1:5000"
 
-registerForm.addEventListener('submit', function (event) {
+document.getElementById('register-form').addEventListener('submit', async function (event) {
     event.preventDefault()
 
-    let formData = {
-        login: document.getElementById('register-login-input').value,
-        email: document.getElementById('register-email-input').value,
-        password: document.getElementById('register-password-input').value
-    }
+    const login = document.getElementById('register-login-input').value
+    const email = document.getElementById('register-email-input').value
+    const password = document.getElementById('register-password-input').value
 
-    fetch(BASE_URL + '/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-    })
-        .then(
-            function (response) {
-                if (response.ok) {
-                    alert('Регистрация прошла успешно!')
-                } else {
-                    alert('Ошибка регистрации: ' + response.status)
-                }
-            }
-        )
-        .catch(function (error) {
-            alert('Произошла ошибка: ' + error)
-        })
+    await register(login, email, password)
 })
+
+document.getElementById('login-form').addEventListener('submit', async function  (event) {
+    event.preventDefault()
+
+    const login = document.getElementById('login-input').value
+    const password = document.getElementById('password-input').value
+
+    await auth(login, password)
+})
+
+async function register(login, email, password) {
+    try {
+        const response = await fetch(BASE_URL + '/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                login: login,
+                email: email,
+                password: password
+            })
+        })
+
+        if (response.ok) {
+            alert('Регистрация прошла успешно!')
+        } else {
+            alert('Ошибка регистрации: ' + response.status)
+        }
+    } catch (error) {
+        alert('Произошла ошибка: ' + error)
+    }
+}
+
+async function auth(login, password) {
+    try {
+        const response = await fetch(BASE_URL + '/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                login: login,
+                password: password
+            })
+        })
+
+        if (response.ok) {
+            const data = await response.json()
+            window.location.href = 'profile.html'
+        } else {
+            alert('Ошибка авторизации: ' + response.status)
+        }
+    } catch (error) {
+        alert('Произошла ошибка: ' + error)
+    }
+}
