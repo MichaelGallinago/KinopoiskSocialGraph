f20654a228cf3460f3fb62e70d16d453620253bd
Diana <lici4qweru090@gmail.com>
2024-07-04 22:47:33 +0500

    made get_logins_statistic + example (comment)

--- client/js/admin.js
+++ client/js/admin.js
@@ -180,6 +180,7 @@ function getRegistrationsStatistic() {
 
 getRegistrationsStatistic();
 
+
 //пример
 /*
 const newUsersData = {
@@ -197,6 +198,85 @@ const newUsersCtx = document.getElementById('newUsersChart').getContext('2d');
 const newUsersChart = new Chart(newUsersCtx, {
   type: 'bar',
   data: newUsersData,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+*/
+
+//getLoginsStatistic
+function getLoginsStatistic() {
+  const data = {
+    start_time: '2024-07-01', // начальная дата для статистики
+    interval_length: 3 // длительность интервала в днях
+  };
+  fetch('/get_logins_statistic', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Failed to get logins statistic.');
+    }
+  })
+  .then(data => {
+      const loginsData = {
+      labels: data.labels,
+      datasets: [{
+        label: 'Логинов',
+        data: data.data,
+        backgroundColor: 'rgba(153, 102, 255, 0.2)',
+        borderColor: 'rgba(153, 102, 255, 1)',
+        borderWidth: 1
+      }]
+    };
+
+    const visitsCtx = document.getElementById('loginsChart').getContext('2d');
+    const visitsChart = new Chart(visitsCtx, {
+      type: 'bar',
+      data: loginsData,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+getLoginsStatistic();
+
+//пример
+/*
+const loginsData = {
+  labels: ["2024-02-01", "2024-02-02", "2024-02-03", "2024-02-04", "2024-02-05", "2024-02-06", "2024-02-07"],
+  datasets: [{
+    label: 'Входов на сайт',
+    data: [12, 19, 3, 5, 2, 3, 7],
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgba(255, 99, 132, 1)',
+    borderWidth: 1
+  }]
+};
+
+const loginsCtx = document.getElementById('visitsChart').getContext('2d');
+const loginsChart = new Chart(loginsCtx, {
+  type: 'line',
+  data: loginsData,
   options: {
     scales: {
       y: {
