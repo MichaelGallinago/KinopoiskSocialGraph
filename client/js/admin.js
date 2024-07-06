e5085691f311f5486206eb12d4299165c49e3a62
Diana <lici4qweru090@gmail.com>
2024-07-06 10:39:23 +0500

    finish traffic statistic i hope

--- client/js/admin.js
+++ client/js/admin.js
@@ -1,6 +1,6 @@
 const BASE_URL = "http://127.0.0.1:5000"
 
-//set_tokens
+//токены
 $(document).ready(() => {
   $('.admin-logout-button').on('click', logout);
   $('.admin-button').on('click', function(event) {
@@ -37,7 +37,7 @@ function logout() {
   window.location.href = 'index.html';
 }
 
-//get_db_statistic
+//статистика бд
 fetch(BASE_URL + '/get_db_statistic')
     .then(response => response.json())
     .then(data => {
@@ -51,7 +51,7 @@ fetch(BASE_URL + '/get_db_statistic')
     });
 
 
-//пример + json файл
+//пример статистиуи бд + json файл
 /*
 let dbStat = {
     "films": 10,
@@ -68,19 +68,15 @@ document.getElementById('db-size-stat').textContent =
 
 */
 
-
-
-
-
-
-//getRegistrationsStatistic новые пользователи
+//пример json-а
 /*
 {
   "start_time": "2023-01-01T00:00:00",
   "interval_length": 1 //промежуток в часах
 }
 */
-// Отправляем POST-запрос на сервер для получения статистики новых пользователей
+
+//новые юзеры
 $.ajax({
   type: 'POST',
   url: BASE_URL + '/get_registrations_statistic',
@@ -90,11 +86,10 @@ $.ajax({
   }),
   contentType: 'application/json',
   success: function(response) {
-    // Получаем данные из ответа сервера
+
     const labels = response.map(item => item[0]);
     const data = response.map(item => item[1]);
 
-    // Создаем график с помощью библиотеки Chart.js
     const ctx = document.getElementById('newUsersChart').getContext('2d');
     const chart = new Chart(ctx, {
       type: 'bar',
@@ -103,7 +98,7 @@ $.ajax({
         datasets: [{
           label: 'Новые пользователи',
           data: data,
-          backgroundColor: 'rgba(75, 192, 192, 1)'
+          backgroundColor: 'rgb(155,127,243)'
         }]
       },
       options: {
@@ -131,109 +126,53 @@ $.ajax({
   }
 });
 
+//посещаемость
+$.ajax({
+  type: 'POST',
+  url: BASE_URL + '/get_logins_statistic',
+  data: JSON.stringify({
+    start_time: '2024-07-01T00:00:00',
+    interval_length: 2
+  }),
+  contentType: 'application/json',
+  success: function(response) {
 
-getRegistrationsStatistic();
+    const labels = response.map(item => item[0]);
+    const data = response.map(item => item[1]);
 
-//пример
-
-const newUsersData = {
-  labels: ["2024-02-01", "2024-02-02", "2024-02-03", "2024-02-04", "2024-02-05", "2024-02-06", "2024-02-07"],
-  datasets: [{
-    label: 'Новых пользователей',
-    data: [12, 19, 3, 5, 2, 3, 7],
-    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-    borderColor: 'rgba(75, 192, 192, 1)',
-    borderWidth: 1
-  }]
-};
-
-const newUsersCtx = document.getElementById('newUsersChart').getContext('2d');
-const newUsersChart = new Chart(newUsersCtx, {
-  type: 'bar',
-  data: newUsersData,
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
-
-
-//getLoginsStatistic посетители
-function getLoginsStatistic() {
-  const data = {
-    start_time: '2024-06-29T14:30:45.123Z', // начальная дата для статистики
-    interval_length: 30 // длительность интервала в днях
-  };
-  fetch(BASE_URL + '/get_logins_statistic', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Failed to get logins statistic.');
-    }
-  })
-  .then(data => {
-      const loginsData = {
-      labels: data.labels,
-      datasets: [{
-        label: 'Логинов',
-        data: data.data,
-        backgroundColor: 'rgba(153, 102, 255, 0.2)',
-        borderColor: 'rgba(153, 102, 255, 1)',
-        borderWidth: 1
-      }]
-    };
-
-    const visitsCtx = document.getElementById('loginsChart').getContext('2d');
-    const visitsChart = new Chart(visitsCtx, {
-      type: 'bar',
-      data: loginsData,
+    const ctx = document.getElementById('visitsChart').getContext('2d');
+    const chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Посещения сайта',
+          data: data,
+          borderColor: 'rgb(101,223,108)',
+          fill: false
+        }]
+      },
       options: {
+        responsive: true,
         scales: {
+          x: {
+            type: 'time',
+            time: {
+              unit: 'day'
+            }
+          },
           y: {
-            beginAtZero: true
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Количество посещений'
+            }
           }
         }
       }
     });
-  })
-  .catch(error => {
+  },
+  error: function(error) {
     console.error(error);
-  });
-}
-
-getLoginsStatistic();
-
-//пример
-const loginsData = {
-  labels: ["2024-02-01", "2024-02-02", "2024-02-03", "2024-02-04", "2024-02-05", "2024-02-06", "2024-02-07"],
-  datasets: [{
-    label: 'Входов на сайт',
-    data: [12, 19, 3, 5, 2, 3, 7],
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    borderColor: 'rgba(255, 99, 132, 1)',
-    borderWidth: 1
-  }]
-};
-
-const loginsCtx = document.getElementById('visitsChart').getContext('2d');
-const loginsChart = new Chart(loginsCtx, {
-  type: 'line',
-  data: loginsData,
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
   }
 });
